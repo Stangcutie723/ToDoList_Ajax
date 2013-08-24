f67f5b7a5368d6891d578b85e8e02da9e459ea88
Stangcutie723 <Stangcutie723@gmail.com>
2013-08-23 17:05:48 -0700

    Ajax around 2

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ToDoListAjax::Application.config.secret_key_base = '80aaa3b5b74f834423ef39711dd30ffef8a2e5508a46a1a6a75638b997c97da3ad6e29f22e76ac22493b38e4cce18ca94cdef8d67c0cbe1b993cd26bfbd19b5b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ToDoListAjax::Application.config.session_store :cookie_store, key: '_ToDoList_Ajax_session'
